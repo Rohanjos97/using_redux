d7dccd8ffdebd42d1acb028c13a995571d718123
Rohanjos97 <Rohanjos97@gmail.com>
2019-12-23 23:11:23 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import EntryPoint from './components/EntryPoint.jsx';
+
+function App() {
+  return (
+    <EntryPoint />
+  );
+}
+
+export default App;
---
+++ src/components/EntryPoint.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class EntryPoint extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            // Write your code here
+        );
+    };
+};
+
+export default EntryPoint;
